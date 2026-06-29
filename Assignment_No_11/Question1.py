f51835954aa2098c29375e46b68d0e47670e9a7d
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-06-29 19:37:43 +0530

    Code Added

--- Assignment_No_11/Question1.py
+++ Assignment_No_11/Question1.py
@@ -1,19 +1,26 @@
 # WAP which accept one number
 # and checks whether it is prime or not
 
-def SOD(No):
-    Sum = 0
+def CheckPrime(No):
+    Count = 0
 
-    while(No//10==0):
-        Sum = Sum + No
+    for i in range(2, No):
+        if No % i == 0:
+            Count = Count + 1
 
-
-
+    return Count
 
 def main():
-    No = int(input("Enter a number :"))
+    Value = int(input("Enter a number: "))
 
-    
+    Result = CheckPrime(Value)
+
+    if Value < 2:
+        print("Number is Not Prime")
+    elif Result == 0:
+        print("Number is Prime")
+    else:
+        print("Number is Not Prime")
 
 if __name__ == "__main__":
     main()
