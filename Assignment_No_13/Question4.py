c78790d68bda886d6f47e51082562b006a963d17
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-06-30 14:19:05 +0530

    Code Added

--- Assignment_No_13/Question4.py
+++ Assignment_No_13/Question4.py
@@ -1,4 +1,19 @@
 # WAP which accept one number and prints its binary equivalent
 
-binary=str(0)+1
-print(binary)
+def Bin_Output(No):
+
+    binary =""
+    while No > 0:
+        binary=str(No%2)+binary
+        No = No // 2
+    return binary
+
+def main():
+    Value = int(input("Enter a number : "))
+
+    Ret = Bin_Output(Value)
+
+    print("Binary equivalent is :", Ret)
+
+if __name__ == "__main__":
+    main()
