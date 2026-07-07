8f1bab096e0c0eaff47b8b29b61641e469d832ce
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-07-07 18:30:16 +0530

    Code Added

---
+++ Assignment_No_14/Question3.py
@@ -0,0 +1,19 @@
+# Write a lambda function which accept two number and return maximum number
+
+Maximum = lambda No1 , No2 : max(No1,No2)
+print(Maximum)
+
+
+
+def main():
+
+    value1 = int(input())
+
+    value2 = int(input())
+
+    Result = Maximum(value1,value2)
+
+    print(f"Among {value1} and {value2} : {Result} is Maximum")
+
+if __name__ == "__main__":
+    main()
