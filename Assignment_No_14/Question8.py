bdf519cf0c1ec9f0884c6f724931d634dfa8a3a1
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-07-07 19:02:23 +0530

    Code Added

---
+++ Assignment_No_14/Question8.py
@@ -0,0 +1,17 @@
+# Write a lambda function which accepts two numbers and return addition
+
+Add = lambda No1 ,No2 : No1 + No2
+print(Add)
+
+def main():
+
+    value1 = int(input("Enter a  number :"))
+
+    value2 = int(input("Enter Second Number :"))
+
+    Result = Add(value1,value2)
+
+    print(f"Addition of {value1} and {value2} is {Result}")
+
+if __name__ == "__main__":
+    main()
