38944b924bae0a1c8ee521a8dec7f977adf23b3e
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-07-10 21:40:28 +0530

    Code Added

---
+++ Assignment_No_16/Question4.py
@@ -0,0 +1,14 @@
+"""WAP which display 5 times Marvellous on screen"""
+
+def main():
+
+    print("To print 5 times marvellous :")
+
+    i = 0
+
+    while (i<5):
+        print("Marvellous")
+        i = i + 1
+
+if __name__ == "__main__":
+    main()
