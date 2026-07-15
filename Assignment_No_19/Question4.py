1178a1a0e275fb18ce2e301f13c47c5fb321d2cd
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-07-15 20:00:40 +0530

    Code Added

---
+++ Assignment_No_19/Question4.py
@@ -0,0 +1,32 @@
+from functools import reduce
+
+def CHK(no):
+    return no%2 == 0
+
+def Inc(no):
+    return no**2
+
+Product = lambda x,y : x+y
+
+def main():
+    Data = []
+
+    print("Number of elements:")
+    No = int(input())
+
+    print("Enter elements:")
+    for i in range(No):
+        Data.append(int(input()))
+
+    Result1 = list(filter(CHK, Data))
+    print("Data after filtr is :",Result1)
+
+    Result2 = list(map(Inc,Result1))
+    print("Data after map is :",Result2)
+
+    Result3 = reduce(Product,Result2)
+    print("Data after reduce is :",Result3)
+
+
+if __name__ == "__main__":
+    main()
