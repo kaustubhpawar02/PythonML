436cc6c59f505f7314ceb6a38b72d7d59d64dc54
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-08-18 19:06:36 +0530

    Code Added

--- Assignment_No_38/Assignment_no_38.py
+++ Assignment_No_38/Assignment_no_38.py
@@ -5,14 +5,29 @@ def Student_Result(filename):
 
     df = pd.read_csv(filename)
 
+    Border = "-"*100
+    print(Border)
+
     # Write a Python program to load the file student_performance_ml.csv using pandas
     # Display :
 
     # First 5 records
     print(df.head())
 
+    print(Border)
+
     # Last 5 records
     print(df.tail())
+
+    print(Border)
+
+    # Total number of rows and columns
+    print("Total no of rows and columns :",df.shape)
+    print("Total no of rows :",df.shape[0])
+    print("Total no of columns :",df.shape[1])
+
+    print(Border)
+
 
 
 def main():
