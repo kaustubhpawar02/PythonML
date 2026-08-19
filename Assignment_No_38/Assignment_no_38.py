031f19b592922976bf7f44615ba2bdbd508853f0
kaustubhpawar02 <Kaustubhpawar0202@gmail.com>
2026-08-19 12:37:13 +0530

    Code Added

--- Assignment_No_38/Assignment_no_38.py
+++ Assignment_No_38/Assignment_no_38.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 def Student_Result(filename):
 
@@ -88,11 +88,79 @@ def Student_Result(filename):
     print("Percentage of Pass Students :",(df["FinalResult"]==1).mean()*100)
     print("Percentage of Fail Students :",(df["FinalResult"]==0).mean()*100)
 
-    print(Border)
-
     # “The dataset is slightly imbalanced, with a 60-40 distribution.”
     
     print(Border)
+
+    # 5. Based on the dataset values,analyze whether :
+    # Higher StudyHours increase the chance of passing
+    # Higher Attendance improves FinalResult.
+    # Write your observations in 4-5 lines
+
+    print(df.groupby("FinalResult")[["StudyHours","Attendance"]].mean())
+    # Students who passed generally have average study hours
+    # Students with higher attendance tend to pass more often.
+    # Lower study hours are associated with more failures.
+    # Attendance appears to positively influence the final result.
+    # Both study hours and attendance contribute to better performance.
+
+    print(Border)
+
+    # 6.Plot a histogram of StudyHours.
+    # Explain what the distribution tells you.
+
+    plt.hist(df["StudyHours"],bins = 10,edgecolor ="black")
+    plt.title("Histogram of study hours")
+    plt.xlabel("Study Hours")
+    plt.ylabel("No of students")
+    plt.show()
+
+    # 7. Create a scatter plot of :
+    # StudyHours vs PreviousScore
+    # Use different colors for Pas and Fail Students.
+
+    colors ={0:"red",1:"green"}
+    plt.scatter(
+        df["StudyHours"],
+        df["PreviousScore"],
+        c = df["FinalResult"].map(colors)
+    )
+
+    plt.xlabel("StudyHours")
+    plt.ylabel("PreviousScore")
+    plt.title("Study Hours v/s PreviouScore")
+    plt.grid(True)
+    plt.show()
+
+    print(Border)
+
+    # 8. Draw a boxplot for Attendance.
+    # Identity if any outliers are present
+
+    plt.boxplot(df["Attendance"])
+    plt.title("Attendance Boxplot")
+    plt.ylabel("Attendance")
+    plt.show()
+
+    print(Border)
+
+    # 9. Create a plot showing the relationship between
+    # AssignmentsCmpleted and FinalResult.
+    # Explain your observation.
+
+    plt.scatter(df["AssignmentsCompleted"],df["FinalResult"])
+    plt.xlabel("Assignment completed ")
+    plt.ylabel("Final Result")
+    plt.title("Assignment completed v/s Final result")
+    plt.show()
+
+    print(Border)
+
+    # 10.Plot SleepHours against FinalResults.
+    # Does sleeping more guarantee success ? Explain.
+
+    
+    
 
 
 
